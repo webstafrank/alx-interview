6e6c9a417443b2e6ff66c2149278b4ba5c8e7401
webstafrank <franklyndenish1@gmail.com>
2024-10-06 14:47:22 -0700

    'Pascal's Triangle'

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,26 +2,17 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Initialize the triangle with the first row
+    triangle = [[1]]  # Start with the first row
 
     for i in range(1, n):
-        prev_row = triangle[-1]  # Get the previous row
-        new_row = [1]  # Start each row with a 1
-
-        # Calculate the values in the middle of the row
-        for j in range(1, len(prev_row)):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-
-        new_row.append(1)  # End each row with a 1
-        triangle.append(new_row)  # Add the new row to the triangle
+        # Start the row with a 1
+        row = [1]
+        # Compute the values for the middle of the row
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        # End the row with a 1
+        row.append(1)
+        triangle.append(row)
 
     return triangle
-
-# Testing the code with n = 5
-if __name__ == "__main__":
-    def print_triangle(triangle):
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-
-    print_triangle(pascal_triangle(5))
 
